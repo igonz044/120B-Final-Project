f1ba575415112be568050f0a44cc937aa1dcf6ff
Itzel <44009297+igonz044@users.noreply.github.com>
2019-06-04 14:37:58 -0700

    Add files via upload

---
+++ 120BFinalProject/ATmega16_Nokia5110_Display_Image/FinalGame.c
@@ -0,0 +1,493 @@
+/*
+ * ATmega16_Nokia5110_Display_Image.c
+ *
+ * http://www.electronicwings.com
+ */ 
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <string.h>
+#include <stdio.h>
+#include <avr/interrupt.h>
+#include "SPI_Master_H_file.h"
+#include "Font.h"
+#include "sadface.h"
+#include "happyface.h"
+#include "nokia5110.c"
+#include "io.c"
+#include "io.h"
+#include "bit.h"
+#include "timer.h"
+#include "scheduler.h"
+
+void N5110_Cmnd(char DATA)
+{
+	PORTB &= ~(1<<DC);									/* make DC pin to logic zero for command operation */
+	SPI_SS_Enable();									/* enable SS pin to slave selection */
+	SPI_Write(DATA);									/* send data on data register */
+	PORTB |= (1<<DC);									/* make DC pin to logic high for data operation */
+	SPI_SS_Disable();
+}
+void N5110_Data(char *DATA)
+{
+	PORTB |= (1<<DC);									/* make DC pin to logic high for data operation */
+	SPI_SS_Enable();									/* enable SS pin to slave selection */
+	int lenan = strlen(DATA);							/* measure the length of data */
+	for (int g=0; g<lenan; g++)
+	{
+		for (int index=0; index<5; index++)
+		{
+			SPI_Write(ASCII[DATA[g] - 0x20][index]);	/* send the data on data register */
+		}
+		SPI_Write(0x00);
+	}
+	SPI_SS_Disable();
+}
+void N5110_Reset()/* reset the Display at the beginning of initialization */
+{
+	PORTB &= ~(1<<RST);
+	_delay_ms(100);
+	PORTB |= (1<<RST);
+}
+void N5110_init()
+{
+	N5110_Reset();										/* reset the display */
+	N5110_Cmnd(0x21);									/* command set in addition mode */
+	N5110_Cmnd(0xC0);									/* set the voltage by sending C0 means VOP = 5V */
+	N5110_Cmnd(0x07);									/* set the temp. coefficient to 3 */
+	N5110_Cmnd(0x13);									/* set value of Voltage Bias System */
+	N5110_Cmnd(0x20);									/* command set in basic mode */
+	N5110_Cmnd(0x0C);									/* display result in normal mode */
+}
+void lcd_setXY(char x, char y)/* set the column and row */
+{
+	N5110_Cmnd(x);
+	N5110_Cmnd(y);
+}
+void N5110_clear()/* clear the Display */
+{
+	SPI_SS_Enable();
+	PORTB |= (1<<DC);
+	for (int k=0; k<=503; k++)
+	{
+		SPI_Write(0x00);
+	}
+	PORTB &= ~(1<<DC);
+	SPI_SS_Disable();
+}
+void N5110_image(const unsigned char *image_data)/* clear the Display */
+{
+	SPI_SS_Enable();
+	PORTB |= (1<<DC);
+	for (int k=0; k<=503; k++)
+	{
+		SPI_Write(image_data[k]);
+	}
+	PORTB &= ~(1<<DC);
+	SPI_SS_Disable();
+}
+
+int height = 84;
+int width = 48;
+int speed = 5;
+int gametick = 0;
+int barStart = 9;
+int barStop = 3;
+
+int grid[48][84] = {
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+};
+
+struct Piece {
+	int x;
+	int y;
+};
+
+struct Piece A = {
+	.x=0,
+	.y=0
+};
+struct Piece B = {
+	.x=16,
+	.y=0
+};
+struct Piece C = {
+	.x=32,
+	.y=0
+};
+
+void babyMode(){
+	speed = 5;
+	barStart = 9;
+	barStop = 3;
+	for(int i = 0; i < width; i++){
+		grid[barStart][i] = 1;
+		grid[barStop][i] = 1;
+	}
+}
+void studentMode(){
+	speed = 3;
+	barStart = 8;
+	barStop = 4;
+	for(int i = 0; i < width; i++){
+		grid[barStart][i] = 1;
+		grid[barStop][i] = 1;
+	}
+}
+void JeffMode(){
+	speed = 1;
+	barStart = 7;
+	barStop = 5;
+	for(int i = 0; i < width; i++){
+		grid[barStart][i] = 1;
+		grid[barStop][i] = 1;
+	}
+}
+
+unsigned char PINAtmp;
+
+//SM1 Declarations
+enum Obstacle_State {init_OS, play_OS, end_OS} OS_State;
+int OS_next;
+unsigned char Position1;
+unsigned char Position2;
+
+//SM2 Declarations
+enum Player_State {init_PS, wait_PS, pressUp_PS, pressDown_PS, release_PS, pause_PS} PS_State;
+int PS_next;
+unsigned char PlayerPosition;
+
+//SM3 Declarations
+enum Display_State {init_DS, check_DS, display_DS, winner_DS, loser_DS, pause_DS} DS_State;
+int DS_next;
+
+int ObstacleTick(int B);
+int PlayerTick(int C);
+int DisplayTick(int D);
+
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//Code for joystick.
+//The reason why
+void InitADC() {
+	ADMUX=(1<<REFS0);                   //setting the reference of ADC such THAT Areff=Avcc
+	ADCSRA=(1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+}
+//used something new datatype her to check if it works or not, unsigned integer short can aolso be written as unint16_t
+
+uint16_t ReadADC(uint8_t ch)
+{	InitADC();
+	ch=ch&0b00000111; //im using this channel to be a extra safety net for the correct reading
+	ADMUX|=ch; // it will read from the first three channels
+	//conversion steps
+	ADCSRA|=(1<<ADSC);
+	while(!(ADCSRA & (1<<ADIF)));
+	ADCSRA|=(1<<ADIF); // got from one of the labs
+	return(ADC);
+}
+//code for reading which direction is the joystick pointing to0
+uint16_t find_direction_from_xy(uint16_t x,uint16_t y)
+{
+	//1-> up, 2-> right, 3-> down 4-> left
+	if(x>255 && x<765 && y>765) // we know 1024 is the max value, these values are exact half of the max values which i had using the joystick
+	return(1); // direction is up
+
+	else if(x>765 && y>255 && y<765)
+	return(2); //direction is right
+
+	else if(x>255 && x<765 && y<255)
+	return(3); //direction is down
+
+	else if(x<255 && y>255 && y<765)
+	return(4); // direction is left
+
+	else
+	return(0); //default state
+}
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+int main()
+{
+	// Set Data Direction Registers
+	// Buttons PORTA[0-7], set AVR PORTA to pull down logic
+	DDRA = 0xFC; PORTA = 0x03;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRC = 0xF0; PORTC = 0x0F; // PC7..4 outputs init 0s, PC3..0 inputs init 1s
+	DDRD = 0xFF; PORTD = 0x00;
+	// Period for the tasks
+
+	//Declare an array of tasks
+	static task task1, task2, task3, task4;
+	task *tasks[] = { &task1, &task2,&task3,&task4};
+	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+	//There was only one task to do so the size = 1;
+
+	task1.state = 0;
+	task1.period = 200; //Task Period.
+	task1.elapsedTime =task1.period;
+	task1.TickFct = &ObstacleTick;
+	
+	task2.state = 0;
+	task2.period = 200; //Task Period.
+	task2.elapsedTime =task2.period;
+	task2.TickFct = &PlayerTick;
+	
+	task3.state = 0;
+	task3.period = 200; //Task Period.
+	task3.elapsedTime =task3.period;
+	task3.TickFct = &DisplayTick;
+
+	// Set the timer and turn it on
+	TimerSet(10);
+	TimerOn();
+	// LCD_init();
+
+	unsigned short i; // Scheduler for-loop iterator
+	SPI_Init();
+	N5110_init();
+	N5110_clear();
+	lcd_setXY(0x40,0x80);
+	/*while(1)
+	{
+		N5110_image(happyface);
+		_delay_ms(1000);
+		N5110_clear();
+		N5110_image(sadface);
+		_delay_ms(1000);
+		nokia_lcd_clear();
+		
+		nokia_position(0,10);
+		N5110_image(copygrid);
+		
+	}
+	return 0;*/
+	while(1) {
+		PINAtmp = ~PINA & 0x1C;
+		for ( i = 0; i < numTasks; i++ ) {
+			if ( tasks[i]->elapsedTime >= tasks[i]->period ) {
+				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+				tasks[i]->elapsedTime = 0;
+			}
+			tasks[i]->elapsedTime += 1;
+		}
+		while(!TimerFlag);
+		TimerFlag = 0;
+	}
+	return 0;
+}
+
+int ObstacleTick(int OS_State)
+{
+	switch(OS_State)
+	{
+		case init_OS:
+		// WELCOME SCREEN
+		// choose difficulty
+		//ifjoystick set 1,2,3 then move to play_os
+		babyMode();
+		studentMode();
+		JeffMode();
+		break;
+		case play_OS:
+		//ACTUAL GAME
+		break;
+		case end_OS:
+		//END SCREEN
+		break;
+		default:
+		break;
+	}
+
+	switch(OS_State)
+	{
+		case init_OS:
+		Position1 = 14;
+		Position2 = 32;
+		break;
+
+		case play_OS:
+		Position1 = Position1 > 1 ? Position1 - 1 : 1;
+		Position2 = Position2 > 1 ? Position2 - 1 : 1;
+		break;
+		
+		case end_OS:
+		Position1 = Position1;
+		Position2 = Position2;
+		break;
+		
+		default:
+		Position1 = 14;
+		Position2 = 32;
+		break;
+	}
+
+	OS_State = OS_next;
+	return OS_State;
+}
+
+//enum Player_State {init_PS, wait_PS, pressUp_PS, pressDown_PS, release_PS, pause_PS} PS_State;
+int PlayerTick(int PS_State)
+{
+	unsigned char UpDown = PINAtmp & 0x18;
+	switch(PS_State)
+	{
+		case init_PS:
+		break;
+
+		default:
+		break;
+	}
+
+	switch(OS_State)
+	{
+		case init_PS:
+		PlayerPosition = 1;
+		break;
+
+		case wait_PS:
+		PlayerPosition++;
+		break;
+
+		case pressUp_PS:
+		PlayerPosition = PlayerPosition + 16;
+		break;
+		
+		case pressDown_PS:
+		PlayerPosition = PlayerPosition - 16;
+		break;
+		
+		case release_PS:
+		PlayerPosition = PlayerPosition;
+		break;
+		
+		case pause_PS:
+		PlayerPosition = PlayerPosition;
+		break;
+
+		default:
+		PlayerPosition = 1;
+		break;
+	}
+	return OS_State;
+}
+
+int DisplayTick(int DS_State)
+{
+	switch(DS_State)
+	{
+		case init_DS:
+
+		break;
+		default:
+		break;
+
+	}
+
+	switch(DS_State)
+	{
+		case init_DS:
+		
+		break;
+		default:
+		break;
+	}
+
+	return DS_State;
+}
+
+/// OBSTACLE:
+/// easy mode:
+///		generate object in random column at slow speed
+/// hard mode:
+/// 	generate object in random column at high speed
+///
+/// INPUT:
+/// buttons 3x
+/// joystick, left - right
+///
+/// DISPLAY:
+
---
+++ 120BFinalProject/ATmega16_Nokia5110_Display_Image/SPI_Master_H_file.h
@@ -0,0 +1,34 @@
+/*
+ * SPI_Master_H_file.h
+ *
+ * http://www.electronicwings.com
+ */ 
+#ifndef SPI_MASTER_H_FILE_H_
+#define SPI_MASTER_H_FILE_H_
+
+#include <avr/io.h>
+
+#define PORT_LCD PORTB
+#define DDR_LCD DDRB
+
+#define SCK  PORTB7//CLk
+#define MOSI PORTB5//DIN
+#define DC   PORTB1//DC
+#define SS   PORTB4//CE
+#define RST  PORTB0
+
+#define MISO PORTB6
+
+// #define LCD_CLK PORTD0
+// #define LCD_DIN PORTD1
+// #define LCD_DC PORTD2
+// #define LCD_SCE PORTD3
+// #define LCD_RST PORTD4
+
+void SPI_SS_Enable();
+void SPI_SS_Disable();
+void SPI_Init();
+void SPI_Write(char write_data);
+char SPI_Read();
+
+#endif /* SPI_MASTER_H_FILE_H_ */
